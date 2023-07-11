bda3d48ec099021bf5b33f88948a9b9d82d087b9
sxdok <72053445+sxdok@users.noreply.github.com>
2023-07-11 11:49:56 +0800

    Update GLaDOS.py

--- GLaDOS.py
+++ GLaDOS.py
@@ -1,27 +1,18 @@
-from os import environ
+import os
 from Check import CheckIn
 from notify import send
-from login import SZLCSC
 
-def main():
-    # 获取actions secrets配置的cookie SendKey
-    ck = os.environ["cookie"]
+# 获取actions secrets配置的cookie SendKey
+ck = os.environ["cookie"]
 
-    notifications = "-----------------------\r\n"
-    my_user.login("GlaDOS")
-    if my_user.GlaDOS:
-        if not ck:
-            notifications += "请先配置GLADOS_COOKIE！"
-            return
+notifications = "-----------------------\r\n"
+if not ck:
+    notifications += "请先配置GLADOS_COOKIE！"
+try:
+    title, Text = CheckIn(ck)
+    notifications += title + "\r\n" + Text
 
-        try:
-            title, Text = CheckIn(ck)
-            notifications += title + "\r\n" + Text
+except Exception as err:
+    notifications += "程序运行出错！"
 
-        except Exception as err:
-            notifications += "程序运行出错！"
-
-    send("GLaDOS 签到通知",notifications)
-
-if __name__ == '__main__':
-    main()
+send("GLaDOS 签到通知",notifications)
