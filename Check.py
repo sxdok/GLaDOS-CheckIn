3a24031200c7af48779ea12bea39e9b5a6a3913e
sxdok <72053445+sxdok@users.noreply.github.com>
2023-11-18 19:09:11 +0800

    修改了代码结构

--- Check.py
+++ Check.py
@@ -1,21 +1,21 @@
 from json import dumps
 from requests import post, get
 
+url = "https://glados.rocks/api/user/checkin"
+url2 = "https://glados.rocks/api/user/status"
+useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0"
 
 def CheckIn(cookie):
-    url = "https://glados.rocks/api/user/checkin"
-    url2 = "https://glados.rocks/api/user/status"
-    referer = 'https://glados.rocks/console/checkin'
-    origin = "https://glados.rocks"
-    useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
-    payload = {
-        'token': 'glados.network'
-    }
+    # referer = 'https://glados.space/console/checkin'
+    origin = "https://glados.space"
+
+    payload = {'token': 'glados.one'}
+
     checkin = post(
         url,
         headers={
             'cookie': cookie,
-            'referer': referer,
+            # 'referer': referer,
             'origin': origin,
             'user-agent': useragent,
             'content-type': 'application/json;charset=UTF-8'
@@ -26,18 +26,21 @@ def CheckIn(cookie):
         url2,
         headers={
             'cookie': cookie,
-            'referer': referer,
+            # 'referer': referer,
             'origin': origin,
             'user-agent': useragent
         }
     )
 
     mess = checkin.json()['message']
-    time = state.json()['data']['leftDays']
-    days = time.split('.')[0]
-    msg = f'checkin: {checkin.status_code} | state: {state.status_code}\n{mess}\n剩余天数：{days}天'
+    # time = state.json()['data']['leftDays']
+    days = state.json()['data']['leftDays'].split('.')[0]
+    # days = time.split('.')[0]
+    # msg = f'checkin: {checkin.status_code} | state: {state.status_code}\n{mess}\n剩余天数：{days}天'
 
     checkin.close()
     state.close()
 
-    return f'{mess}',f'剩余{days}天'
+    # print(f'{mess}\n剩余天数:{days}天')
+
+    return f'{mess}\n剩余天数:{days}天'
